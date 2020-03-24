c2b9fb619299146831ed8366d15bbcbc8a798eeb
John Hakala <john_hakala@virginia.edu>
2020-03-24 17:37:48 -0500

    remove debugging cout

--- Ntuplizer/plugins/JetsNtuplizer.cc
+++ Ntuplizer/plugins/JetsNtuplizer.cc
@@ -566,10 +566,10 @@ void JetsNtuplizer::fillBranches( edm::Event const & event, const edm::EventSetu
       nBranches_->jetAK8_charge 	    .push_back(fj.charge());					 
       nBranches_->jetAK8_HbbtagOLD          .push_back(fj.bDiscriminator("pfBoostedDoubleSecondaryVertexAK8BJetTags"));       
       nBranches_->jetAK8_DDB                .push_back(fj.bDiscriminator("pfDeepDoubleBvLJetTags:probHbb"));       
-      std::cout << "test " << fj.bDiscriminator("pfMassIndependentDeepDoubleBvLJetTags:probHbb") << std::endl;
-      for (unsigned short i = 0; i<fj.getPairDiscri().size(); ++i) {
-        std::cout << fj.getPairDiscri().at(i).first << std::endl;
-      }
+      //std::cout << "test " << fj.bDiscriminator("pfMassIndependentDeepDoubleBvLJetTags:probHbb") << std::endl;
+      //for (unsigned short i = 0; i<fj.getPairDiscri().size(); ++i) {
+      //  std::cout << fj.getPairDiscri().at(i).first << std::endl;
+      //}
       nBranches_->jetAK8_decDDB             .push_back(fj.bDiscriminator("pfMassIndependentDeepDoubleBvLJetTags:probHbb"));       
       nBranches_->jetAK8_csv                .push_back(fj.bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags"));
       nBranches_->jetAK8_deep_csv_b         .push_back(fj.bDiscriminator("pfDeepCSVJetTags:probb") );
